6c6a2f9196ff503a9922f68a3e7a28a3e0675b73
wenc <wncbtrnfl@gmail.com>
2024-11-10 16:20:13 +0800

    change marketplace list layout

--- src/views/child/sellProduct.vue
+++ src/views/child/sellProduct.vue
@@ -3,22 +3,22 @@
         <h1 class="text-lg">Create a listing</h1>
         <div>
             <p v-if="hasEmptyFields" class="bg-red-500 pl-2 rounded mb-2 py-1" >Input product details</p>
-            <form @submit.prevent="createListing" class="flex flex-col gap-y-7">
+            <form @submit.prevent="createListing" class="grid md:grid-cols-2 gap-7">
                 <div class="flex flex-col gap-y-2">
                     <label>Product Name</label>
-                    <input type="text" class="w-2/4 h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2" v-model="productData.productName">
+                    <input type="text" class="w-full h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2" v-model="productData.productName">
                 </div>
                 <div class="flex flex-col gap-y-2">
                     <label>Product Description</label>
-                    <textarea type="text" class="w-2/4 min-h-16 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none p-2" v-model="productData.productDescription"></textarea>
+                    <textarea type="text" class="w-full min-h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none p-2" v-model="productData.productDescription"></textarea>
                 </div>
                 <div class="flex flex-col gap-y-2">
                     <label>Product Price</label>
-                    <input type="number" class="w-2/4 h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2" v-model="productData.productPrice">
+                    <input type="number" class="w-full h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2" v-model="productData.productPrice">
                 </div>
                 <div class="flex flex-col gap-y-2">
                     <label>Product Category</label>
-                    <select v-model="productData.productCategory" class="w-2/4 h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2 dark:text-gray-300">
+                    <select v-model="productData.productCategory" class="w-full h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2 dark:text-gray-300">
                         <option value="">Select Category</option>
                         <option class="dark:text-black">Writing Instruments</option>
                         <option class="dark:text-black">Paper Products</option>
@@ -30,17 +30,17 @@
                 </div>
                 <div class="flex flex-col gap-y-2">
                     <label>Product Quantity</label>
-                    <input type="number" class="w-2/4 h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2" v-model="productData.productQuantity">
+                    <input type="number" class="w-full h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2" v-model="productData.productQuantity">
                 </div>
                 <div class="flex flex-col gap-y-2">
                     <label>Product Image <span class="text-xs">(Atleast one image)</span></label>
-                    <div @click="toggleImageInput" :class="{ '!cursor-not-allowed': tempUrl.length === 3 }" class="w-2/4 h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2 flex items-center justify-center gap-x-2 cursor-pointer">
+                    <div @click="toggleImageInput" :class="{ '!cursor-not-allowed': tempUrl.length === 3 }" class="w-full h-8 rounded border border-gray-300 dark:border-gray-100/10 dark:bg-transparent focus:outline-none pl-2 flex items-center justify-center gap-x-2 cursor-pointer">
                         <Icon icon="material-symbols:imagesmode-outline" class="dark:text-gray-100/55 text-2xl cursor-pointer hover:text-gray-600 hover:dark:text-gray-100/75" />
                         <p class="text-sm">Choose product image</p>
                     </div>
                     <input id="imageInput" type="file" class="hidden" accept=".jpg, .png, .jpeg" @change="handleImageUpload" multiple :disabled="tempUrl.length === 3">
                 </div>
-                <div class="grid grid-cols-3 gap-x-2">
+                <div class="grid grid-cols-4 gap-x-2 col-span-2">
                     <div v-for="(image, index) in tempUrl" :key="index" class="w-full relative group">
                         <img :src="image" alt="product image" class="w-full object-cover !aspect-square rounded">
                         <div class="bg-black/55 w-fit p-1 rounded-full absolute top-2 right-2 hidden group-hover:block cursor-pointer" @click="removeImage(index)">
@@ -48,7 +48,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="flex justify-end my-5">
+                <div class="flex justify-end my-5 col-span-2">
                     <button v-if="!creatingList" class="bg-blue-500 rounded w-fit px-3 py-1">Create a listing</button>
                     <button v-else class="bg-blue-500 rounded w-fit px-3 py-1 animate-pulse" disabled>Creating..</button>
                 </div>
