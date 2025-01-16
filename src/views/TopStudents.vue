3858587dc05ea657de61143ea21fb9b5a54b102b
wenc <wncbtrnfl@gmail.com>
2025-01-17 01:00:35 +0800

    added: pagination

--- src/views/TopStudents.vue
+++ src/views/TopStudents.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="flex flex-col space-y-5 mb-5 content">
         <h1 class="text-lg capitalize">Top Users</h1>
+
+        <!-- Top Collaborators Section -->
         <div class="bg-gray-100/10 p-5 rounded-md space-y-3 flex flex-col">
             <h1>Top Collaborators</h1>
             <table class="w-full cursor-pointer">
@@ -12,16 +14,30 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(collaborator, index) in filteredTopCollaborators()" :key="collaborator.id">
-                        <td class="pl-2 py-2">{{ index + 1 }}</td>
+                    <tr v-for="(collaborator, index) in paginatedTopCollaborators" :key="collaborator.id">
+                        <td class="pl-2 py-2">{{ index + 1 + (currentCollaboratorPage - 1) * rowsPerPage }}</td>
                         <td class="pl-2 py-2 capitalize">{{ collaborator.displayName }}</td>
                         <td class="pl-2 py-2 text-center">{{ collaborator.collabs?.length }}</td>
                     </tr>
-                </tbody>    
+                </tbody>
             </table>
-            <button v-if="topCollaborators.length > selectFilter" class="mx-auto hover:underline"  @click="selectFilter += 100">See more</button>
-            <button v-else class="mx-auto hover:underline"  @click="selectFilter -= 100">See less</button>
+            <div class="flex justify-end space-x-2 mt-3">
+                <button 
+                    class="px-3 py-1 bg-gray-300 rounded text-black hover:bg-gray-400"
+                    :disabled="currentCollaboratorPage === 1"
+                    @click="currentCollaboratorPage--">
+                    <Icon icon="weui:arrow-filled" class="text-2xl rotate-180" />
+                </button>
+                <button 
+                    class="px-3 py-1 bg-gray-300 rounded text-black hover:bg-gray-400"
+                    :disabled="currentCollaboratorPage === totalCollaboratorPages"
+                    @click="currentCollaboratorPage++">
+                    <Icon icon="weui:arrow-filled" class="text-2xl" />
+                </button>
+            </div>
         </div>
+
+        <!-- Most Rated Section -->
         <div class="bg-gray-100/10 p-5 rounded-md space-y-3 flex flex-col">
             <h1>Most Rated</h1>
             <table class="w-full cursor-pointer">
@@ -33,15 +49,27 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(collaborator, index) in filteredMostRateds()" :key="collaborator.id">
-                        <td class="pl-2 py-2">{{ index + 1 }}</td>
+                    <tr v-for="(collaborator, index) in paginatedMostRateds" :key="collaborator.id">
+                        <td class="pl-2 py-2">{{ index + 1 + (currentMostRatedPage - 1) * rowsPerPage }}</td>
                         <td class="pl-2 py-2 capitalize">{{ collaborator.displayName }}</td>
                         <td class="pl-2 py-2 text-center">{{ collaborator.averageRating }}</td>
                     </tr>
-                </tbody>    
+                </tbody>
             </table>
-            <button v-if="topRatedUsers.length > filterMostRated" class="mx-auto hover:underline"   @click="filterMostRated += 100">See more</button>
-            <button v-else class="mx-auto hover:underline"  @click="filterMostRated -= 100">See less</button>
+            <div class="flex justify-end space-x-2 mt-3">
+                <button 
+                    class="px-3 py-1 bg-gray-300 rounded text-black hover:bg-gray-400"
+                    :disabled="currentMostRatedPage === 1"
+                    @click="currentMostRatedPage--">
+                    <Icon icon="weui:arrow-filled" class="text-2xl rotate-180" />
+                </button>
+                <button 
+                    class="px-3 py-1 bg-gray-300 rounded text-black hover:bg-gray-400"
+                    :disabled="currentMostRatedPage === totalMostRatedPages"
+                    @click="currentMostRatedPage++">
+                    <Icon icon="weui:arrow-filled" class="text-2xl" />
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -54,17 +82,23 @@ const dataStore = useDataStore()
 const topCollaborators = computed(() => dataStore.topCollaborators)
 const topRatedUsers = computed(() => dataStore.topRatedUsers)
 
-const selectFilter = ref(100)
+const rowsPerPage = 15
 
-const filteredTopCollaborators = () => {
-    return topCollaborators?.value.slice(0, selectFilter.value)
-}
+// Pagination for Top Collaborators
+const currentCollaboratorPage = ref(1)
+const totalCollaboratorPages = computed(() => Math.ceil(topCollaborators.value.length / rowsPerPage))
+const paginatedTopCollaborators = computed(() => {
+    const start = (currentCollaboratorPage.value - 1) * rowsPerPage
+    return topCollaborators.value.slice(start, start + rowsPerPage)
+})
 
-const filterMostRated = ref(100)
-
-const filteredMostRateds = () => {
-    return topRatedUsers?.value.slice(0, filterMostRated.value)
-}
+// Pagination for Most Rated
+const currentMostRatedPage = ref(1)
+const totalMostRatedPages = computed(() => Math.ceil(topRatedUsers.value.length / rowsPerPage))
+const paginatedMostRateds = computed(() => {
+    const start = (currentMostRatedPage.value - 1) * rowsPerPage
+    return topRatedUsers.value.slice(start, start + rowsPerPage)
+})
 
 onMounted(() => {
     dataStore.getStudents()
@@ -75,5 +109,4 @@ onMounted(() => {
 .content::-webkit-scrollbar {
     display: none;
 }
-
 </style>
